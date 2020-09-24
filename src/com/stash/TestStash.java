580ef61f113174c716eebbb4082f7c8449ecd5b7
olgazh86 <volhazhmurko@gmail.com>
2020-09-24 19:59:43 -0400

    First stash commit

--- src/com/stash/TestStash.java
+++ src/com/stash/TestStash.java
@@ -1,5 +1,10 @@
 package com.stash;
 
 public class TestStash {
+	
+	public static void main(String[] args) {
+		System.out.println("Modification of our code");
+		System.out.println("Hello World");
+	}
 
 }
