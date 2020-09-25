765912892f3e55b3cf330ae4e42dc3a132dac4fa
olgazh86 <volhazhmurko@gmail.com>
2020-09-24 20:04:31 -0400

    Git stash pop added

--- src/com/stash/TestStash.java
+++ src/com/stash/TestStash.java
@@ -5,6 +5,8 @@ public class TestStash {
 	public static void main(String[] args) {
 		System.out.println("Modification of our code");
 		System.out.println("Hello World");
+		
+		System.out.println("git stash pop");
 	}
 
 }
